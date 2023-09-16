c1f3c537196e910f4dddb080f10b7fba63ab6e22
danig <dani.grunin@gmail.com>
2023-09-16 12:41:46 +0300

    detects but didn't show the rectangles

--- apps/home/views.py
+++ apps/home/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 import cv2
+from ultralytics import YOLO
+
 from yolo_engine import video_detection
 from django.http import StreamingHttpResponse
 from django.shortcuts import render
@@ -71,16 +73,43 @@ def generate_frames_webcam(path_x):
     cap.release()
 
 
+yolo = YOLO('yolov8n.pt')
+
+def draw_boxes(frame, detections):
+    for detection in detections:
+        x1, y1, x2, y2, class_id, conf = detection
+
+        # Convert coordinates to integers
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+        # Draw bounding box
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+        # Add class label and confidence score
+        label = f'Class {int(class_id)}: {conf:.2f}'
+        cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    return frame
+
+
 def stream():
     cap = cv2.VideoCapture(0)
+
     while True:
         ret, frame = cap.read()
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
+
+        # Perform object detection using YOLOv8
+        detections = yolo(frame)
+
+
+        # Convert the frame to JPEG format
         image_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
+
         yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + image_bytes + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + image_bytes + b'\r\n')
 
 
 def video_feed(request):
