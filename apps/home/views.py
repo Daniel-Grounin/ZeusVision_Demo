116b8e0c171bb3ccf0dc863477263b495951d9bd
danig <dani.grunin@gmail.com>
2023-09-16 12:53:36 +0300

    detects and also show the rectangles

--- apps/home/views.py
+++ apps/home/views.py
@@ -47,70 +47,16 @@ def pages(request):
 
 
 
-
 def generate_frames_webcam(path_x):
-    # YOLO processing code
-    cap = cv2.VideoCapture(path_x)
-
-    while True:
-        success, frame = cap.read()
-
-        if not success:
-            break
-
-        # Perform YOLO object detection on the frame
-        yolo_output = video_detection(frame)
-
-        # Convert the detected frame to JPEG format
-        _, buffer = cv2.imencode('.jpg', yolo_output)
-        frame_bytes = buffer.tobytes()
-
-        # Yield the frame in the format required for multipart/x-mixed-replace
+    yolo_output = video_detection(path_x)
+    for detection_ in yolo_output:
+        ref, buffer = cv2.imencode('.jpg', detection_)
+        frame = buffer.tobytes()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-    # Release the video capture object when done
-    cap.release()
-
-
-yolo = YOLO('yolov8n.pt')
-
-def draw_boxes(frame, detections):
-    for detection in detections:
-        x1, y1, x2, y2, class_id, conf = detection
-
-        # Convert coordinates to integers
-        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-
-        # Draw bounding box
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-        # Add class label and confidence score
-        label = f'Class {int(class_id)}: {conf:.2f}'
-        cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-    return frame
-
-
-def stream():
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-
-        # Perform object detection using YOLOv8
-        detections = yolo(frame)
-
-
-        # Convert the frame to JPEG format
-        image_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + image_bytes + b'\r\n')
 
 
 def video_feed(request):
-    return StreamingHttpResponse(stream(), content_type='multipart/x-mixed-replace; boundary=frame')
+    return StreamingHttpResponse(generate_frames_webcam(path_x=0), content_type='multipart/x-mixed-replace; boundary=frame')
+
