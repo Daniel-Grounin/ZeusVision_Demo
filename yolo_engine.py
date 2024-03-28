975180f26d3ed368ef49649486691e15ccb2fae4
Dori Fourer <dori.fourer654@gmail.com>
2024-03-28 13:30:49 +0200

    fix index , add best.pt to detect tanks, and yolo_engine.py update

--- yolo_engine.py
+++ yolo_engine.py
@@ -5,6 +5,7 @@ import cv2
 import math
 from django.core.cache import cache
 
+
 def video_detection(path_x):
     video_capture = path_x
     # Create a Webcam Object
@@ -13,17 +14,8 @@ def video_detection(path_x):
     frame_height = int(cap.get(4))
     # out=cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P','G'), 10, (frame_width, frame_height))
 
-    model = YOLO("../ZeusVision_Demo/yolov8n.pt")
-    classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-                  "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-                  "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-                  "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-                  "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-                  "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-                  "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-                  "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-                  "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-                  "teddy bear", "hair drier", "military vehicle"
+    model = YOLO("../ZeusVision_Demo/best.pt")
+    classNames = ["tank", "person", "teddy bear", "0", "1"
                   ]
     while True:
         success, img = cap.read()
@@ -33,7 +25,7 @@ def video_detection(path_x):
             for box in boxes:
                 x1, y1, x2, y2 = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                #print(x1, y1, x2, y2)
+                # print(x1, y1, x2, y2)
                 cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
                 conf = math.ceil((box.conf[0] * 100)) / 100
                 cls = int(box.cls[0])
@@ -46,8 +38,8 @@ def video_detection(path_x):
                     cache.set('detections', detections, timeout=300)  # Adjust timeout as needed
                 label = f'{class_name}{conf}'
                 t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
-                #print(t_size)
-                #print(class_name)
+                # print(t_size)
+                # print(class_name)
                 c2 = x1 + t_size[0], y1 - t_size[1] - 3
                 cv2.rectangle(img, (x1, y1), c2, [255, 0, 255], -1, cv2.LINE_AA)  # filled
                 cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
